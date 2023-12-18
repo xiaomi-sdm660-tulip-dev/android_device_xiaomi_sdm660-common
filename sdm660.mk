945864e55a6009d40415103dda9ecd8940815015
NopeNopeGuy <kingdutchisbad@gmail.com>
2023-12-18 19:23:00 +0530

    Spoof fingerprint to fix CTS and Play Integrity

--- sdm660.mk
+++ sdm660.mk
@@ -484,5 +484,7 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 # leave less information available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+BUILD_FINGERPRINT := acer/b1-780_ww_gen1/acer_barricadewifi:6.0/MRA58K/1481784106:user/release-keys
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
