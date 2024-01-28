dc0a4fdddae3ba4d0f3cf71612d32fde9be18bce
Nem1x <nemanjanedeljkovic738@gmail.com>
2024-01-28 22:17:16 +0000

    Revert "Change sdm660-common to bouquet-common"
    This reverts commit 2ae79f97bbe8f807d9609ad866532715e00b7067.

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -20,7 +20,7 @@ import os
 import sys
 from hashlib import sha1
 
-DEVICE = 'bouquet-common'
+DEVICE = 'sdm660-common'
 VENDOR = 'xiaomi'
 VENDOR_PATH = os.path.join(
     *['..', '..', '..', 'vendor', VENDOR, DEVICE, 'proprietary'])
